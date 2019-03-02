f6da02c6ebe5091c6a1ad8c44f1e057cdd3e79ed
zeggler <zachery@pixels3dtours.com>
2019-03-01 23:52:23 -0500

    finished clear form functionality

--- src/components/card.js
+++ src/components/card.js
@@ -3,30 +3,34 @@ import React, { Component } from 'react';
 import Input from './input';
 import Content from './content';
 
+const INITIAL_STATE = {
+    color: '',
+    pluralNoun: '',
+    adjectiveOne: '',
+    celebOne: '',
+    adjectiveTwo: '',
+    nounOne: '',
+    numberOne: '',
+    numberTwo: '',
+    nounTwo: '',
+    adjectiveThree: '',
+    celebTwo: '',
+    celebThree: '',
+    adjectiveFour: '',
+    nounThree: '',
+    celebFour: '',
+    adjectiveFive: '',
+    contentVisible: false
+}
+
+
 class Card extends Component {
 
     constructor() {
         super()
 
-        this.state = {
-            color: '',
-            pluralNoun: '',
-            adjectiveOne: '',
-            celebOne: '',
-            adjectiveTwo: '',
-            nounOne: '',
-            numberOne: '',
-            numberTwo: '',
-            nounTwo: '',
-            adjectiveThree: '',
-            celebTwo: '',
-            celebThree: '',
-            adjectiveFour: '',
-            nounThree: '',
-            celebFour: '',
-            adjectiveFive: '',
-            contentVisible: false
-        }
+        this.state = INITIAL_STATE;
+    
 
         this.handleInputChange = this.handleInputChange.bind(this);
         this.handleFormSubmit = this.handleFormSubmit.bind(this);
@@ -39,7 +43,12 @@ handleInputChange(event) {
 
 handleFormSubmit(event) {
     event.preventDefault()
-    this.setState( { contentVisible: !this.state.contentVisible } )
+   
+   if(this.state.contentVisible) {
+    this.setState(INITIAL_STATE)
+   } else  {
+    this.setState( { contentVisible: true } )
+   }
 }
 
 
